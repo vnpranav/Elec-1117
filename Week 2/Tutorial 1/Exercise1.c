3fab86e1a46ff0bcf74827e7b854282d20aed95d
pranav <v.pranav.n@gmail.com>
2024-07-29 14:41:17 +0400

    week 2 tutorial

---
+++ Week 2/Tutorial 1/Exercise1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(void){
+   /*a*/
+   int c, thisVariable, q76354, number;
+
+   /*b*/
+   int a;
+   printf("Enter a number: ");
+   scanf("%d", &a);
+   printf("%d", a);
+}
